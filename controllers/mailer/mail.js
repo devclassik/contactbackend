fb934ad38814de0d839d2dae47e4ac46656fd76a
Opemipo <alomajaopemipo8@gmail.com>
2024-05-18 12:28:52 +0100

    fix mail

--- controllers/mailer/mail.js
+++ controllers/mailer/mail.js
@@ -10,27 +10,58 @@ const transporter = nodemailer.createTransport({
 });
 
 const sendMailer = asyncHandler(async (req, res) => {
-  // console.error('data 1',req.body);
+  const { name, email, to, subject, message } = req.body;
 
-  const mailOptions = {
-    from: req.body.email,
-    to: req.body.to,
-    subject: req.body.subject,
-    html: req.body.message,
+  // Create the email body for the recipient
+  const recipientMailBody = `
+    <p>Hello ${to},</p>
+    <p>You have received a message from ${name} (${email}):</p>
+    <p>${message}</p>
+    <p>Regards,</p>
+    <p>${name}</p>
+  `;
+
+  // Create the email body for the sender
+  const senderMailBody = `
+    <p>Hello ${name},</p>
+    <p>You have sent the following message to ${to} (${email}):</p>
+    <p>${message}</p>
+    <p>Kindly wait for response</p>
+    <p>Regards,</p>
+    <p>Opemipo Alomaja</p>
+  `;
+
+  // Mail options for the recipient
+  const recipientMailOptions = {
+    from: email,
+    to: to, // Allow any email address to receive the message
+    subject: subject,
+    html: recipientMailBody,
   };
 
-  // console.error('data',mailOptions);
-  await transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      // console.log('error 1', error);
+  // Mail options for the sender
+  const senderMailOptions = {
+    from: email,
+    to: email, // Send a copy to the sender
+    subject: subject,
+    html: senderMailBody,
+  };
 
+  // Send email to the recipient
+  await transporter.sendMail(recipientMailOptions, (error, info) => {
+    if (error) {
       return res.status(500).send(error);
     }
-    // console.log('success', info);
+  });
 
-    res.status(200).json({msg:info}).send("Email sent successfully");
+  // Send email to the sender
+  await transporter.sendMail(senderMailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).send(error);
+    }
+
+    res.status(200).json({ msg: info }).send("Email sent successfully");
   });
 });
-
 
 module.exports = { sendMailer };
