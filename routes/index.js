022f900e77f1426660de5f3edca78741e3c4f74a
Opemipo <alomajaopemipo8@gmail.com>
2024-05-31 15:32:56 +0100

    mail route

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express();
 
 const BlogRoute = require("./blog/blog");
+const mailRoute = require("./mail/mail");
 
 
 router.use("/blog", BlogRoute);
+router.use("/mail", mailRoute);
 // router.use("/search", search);
 
 module.exports = router;
